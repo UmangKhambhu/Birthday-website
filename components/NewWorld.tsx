11549a1deb6d3f63e6d0ff9a565b81216ec95a24
Umang Khambhu <umang05.khambhu@gmail.com>
2026-09-14 11:34:43 +0530

    Update NewWorld.tsx

--- components/NewWorld.tsx
+++ components/NewWorld.tsx
@@ -80,17 +80,17 @@ function CardProvider({ children }: { children: React.ReactNode }) {
 
   const cards: Card[] = [
     // { id: "1", imageUrl: "/ref/💜.jpg", alt: "Cutie", title: "Cutie" },
-    { id: "2", imageUrl: "/ref/girl.jpg", alt: "Baddie", title: "Baddie" },
+    { id: "2", imageUrl: "/ref/girl.jpg", alt: "", title: "" },
     { id: "3", imageUrl: "/ref/girl2.jpg", alt: "Angel", title: "Angel" },
     { id: "4", imageUrl: "/ref/girl3.jpg", alt: "Dreamgirl", title: "Dreamgirl" },
-    { id: "5", imageUrl: "/ref/girl4.jpg", alt: "Sweetheart", title: "Sweetheart" },
-    { id: "6", imageUrl: "/ref/girl5.jpg", alt: "Icon", title: "Icon" },
-    { id: "7", imageUrl: "/ref/girl6.jpg", alt: "Stunner", title: "Stunner" },
-    { id: "8", imageUrl: "/ref/girl7.jpg", alt: "Babe", title: "Babe" },
+    { id: "5", imageUrl: "/ref/girl4.jpg", alt: "Sweetudii", title: "Sweetudii" },
+    { id: "6", imageUrl: "/ref/girl5.jpg", alt: "", title: "" },
+    { id: "7", imageUrl: "/ref/girl6.jpg", alt: "", title: "" },
+    { id: "8", imageUrl: "/ref/girl7.jpg", alt: "Babudi", title: "Babudi" },
     { id: "9", imageUrl: "/ref/girl8.jpg", alt: "Darling", title: "Darling" },
-    { id: "10", imageUrl: "/ref/girl9.jpg", alt: "Sunshine", title: "Sunshine" },
+    { id: "10", imageUrl: "/ref/girl9.jpg", alt: "Moon", title: "Moon" },
     { id: "11", imageUrl: "/ref/girl10.jpg", alt: "Queen", title: "Queen" },
-    { id: "12", imageUrl: "/ref/girl11.jpg", alt: "Heartbreaker", title: "Heartbreaker" },
+    { id: "12", imageUrl: "/ref/girl11.jpg", alt: "", title: "" },
   ]
 
   // const cards: Card[] = [
